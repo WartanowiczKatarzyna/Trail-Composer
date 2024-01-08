e6b02e230eb7ea59bc4f7116b64a0e60355dc481
s22074 <101325477+s22074@users.noreply.github.com>
2024-01-08 20:42:20 +0100

    added poi photo controller

---
+++ Trail Composer/Trail Composer/Controllers/PhotoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Trail_Composer.Data;
+
+namespace Trail_Composer.Controllers
+{
+    [Route("tc-api/poi-photo")]
+    [ApiController]
+    public class PhotoController : ControllerBase
+    {
+        private readonly TrailComposerDbContext _context;
+
+        public PhotoController(TrailComposerDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetImage(int id)
+        {
+            // Assuming you have an entity named ImageEntity with an Id property
+            var photo = await _context.Poiphotos.FindAsync(id);
+
+            if (photo == null)
+            {
+                return NotFound();
+            }
+
+            // Retrieve image data from the entity
+            byte[] imageData = photo.Photo;
+
+            // Return the image as a FileResult
+            return File(imageData, "image/jpeg"); // Adjust the content type based on your image type
+        }
+    }
+
+
+}
