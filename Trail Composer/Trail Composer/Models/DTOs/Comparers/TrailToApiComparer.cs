5620beb8dcc188113abbe6d9b0a1a2633227334e
WartanowiczKatarzyna <wartanowiczk@gmail.com>
2024-05-10 14:08:58 +0200

    TrailToApi DTO + Comparer

--- Trail Composer/Trail Composer/Models/DTOs/Comparers/TrailToApiComparer.cs
+++ Trail Composer/Trail Composer/Models/DTOs/Comparers/TrailToApiComparer.cs
@@ -2,5 +2,24 @@
 {
     public class TrailToApiComparer
     {
+        public bool Equals(TrailToApi? x, TrailToApi? y)
+        {
+            if (ReferenceEquals(x, y))
+            {
+                return true;
+            }
+
+            if (x is null || y is null)
+            {
+                return false;
+            }
+
+            return x.Id == y.Id;
+        }
+
+        public int GetHashCode(TrailToApi obj)
+        {
+            return obj.Id.GetHashCode();
+        }
     }
 }
--- Trail Composer/Trail Composer/Models/DTOs/TrailToApi.cs
+++ Trail Composer/Trail Composer/Models/DTOs/TrailToApi.cs
@@ -2,5 +2,32 @@
 {
     public class TrailToApi
     {
+        public required int Id { get; set; }
+        public string TcuserId { get; set; }
+        public required string Name { get; set; }
+        public string Username { get; set; }
+        public int LevelId { get; set; }
+        public int TotalLength { get; set; }
+        public string? Description { get; set; }
+        public ICollection<int> SegmentIds { get; set; } = new HashSet<int>();
+        public ICollection<int> CountryIds { get; set; } = new HashSet<int>();
+        public ICollection<int> PathTypeIds { get; set; } = new HashSet<int>();
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || !(obj is TrailToApi))
+            {
+                return false;
+            }
+
+            var other = (TrailToApi)obj;
+            return this.Id == other.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
     }
+}
 }
